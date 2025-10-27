bf66b09d752c7b73e674d9d3748ab3e0bdc405c8
chris zheng <chengivven@gmail.com>
2025-10-27 10:25:31 +0800

    hw

--- 猜數字遊戲/Form1.Designer.cs
+++ 猜數字遊戲/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(58, 39);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "start game";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(58, 103);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "看答案";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(58, 266);
+            textBox1.MaxLength = 4;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(422, 42);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(323, 251);
+            listBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(41, 355);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "try";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(190, 359);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 5;
+            button4.Text = "re";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(84, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 19);
+            label1.TabIndex = 6;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button button3;
+        private Button button4;
+        private Label label1;
     }
 }
--- 猜數字遊戲/Form1.cs
+++ 猜數字遊戲/Form1.cs
@@ -1,10 +1,62 @@
+using System.Security.Cryptography;
+
 namespace 猜數字遊戲
 {
     public partial class Form1 : Form
     {
+        string answer = "";
         public Form1()
         {
+           
             InitializeComponent();
+        }
+        private void StartNewGame()
+        {
+            Random rnd = new Random();
+            answer = "";
+
+            var ans = rnd.Next(1, 10000);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        } 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string CheckGuess(string guess)
+        {
+            int A = 0; // 數字和位置都對
+            int B = 0; // 數字對但位置錯
+
+            // 逐一比較4位數字
+            for (int i = 0; i < 4; i++)
+            {
+                if (guess[i] == ans[i])
+                {
+                    A++; // 相同位置數字相同 → A
+                }
+                else if (ans.Contains(guess[i]))
+                {
+                    B++; // 答案中有這個數字但位置不同 → B
+                }
+            }
+
+            // 回傳結果文字，例如 "1A2B"
+            return $"{A}A{B}B";
         }
     }
 }
